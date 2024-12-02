6c81470c714475d61e85a8151c28cd0ee9ad8ee5
Jeyanth <113585649+Jeyanthn@users.noreply.github.com>
2024-12-03 00:42:36 +0530

    First Commit

--- src/Database.java
+++ src/Database.java
@@ -2,8 +2,8 @@ import java.sql.*;
 
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/booking";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "Jeya";
+    private static final String USERNAME = "Your username";
+    private static final String PASSWORD = "Your password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
